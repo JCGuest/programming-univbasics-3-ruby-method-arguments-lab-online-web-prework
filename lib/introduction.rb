dc32ddbb830c65bbf50563ed5b0702a03aed2f2b
John Guest <jcguest90@gmail.com>
2019-07-22 04:45:42 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -13,8 +13,3 @@ def introduction_with_language_optional(name, language)
 end
 introduction_with_language_optional("Jo", "Ruby")
 
-
-def introduction_with_language_optional(name, language)
-  p "Hi, my name is #{name} and I am learning to program in #{language}."
-end
-introduction_with_language_optional("Jo", "Ruby")
